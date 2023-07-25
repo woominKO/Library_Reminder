45232090b8ce769dca19da5d38cb6a3d8cc74544
woominKO <woomin422@gmail.com>
2023-07-26 08:43:57 +0900

    add notice

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ const button = document.querySelector('button');
 
 function Timer() {
   var now1 = new Date().getTime();
-  //   var countDownDate = now1 + 3600000 * 3;
+  // var countDownDate = now1 + 3600000 * 3;
   var countDownDate = now1 + 10000;
   console.log(countDownDate);
+  //Notification
+  Notification.requestPermission().then((perm) => {
+    if (perm === 'granted') {
+      var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
 
-  var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.querySelector('div').innerHTML =
+          hours + 'h ' + minutes + 'm ' + seconds + 's ';
 
-    document.querySelector('div').innerHTML =
-      hours + 'h ' + minutes + 'm ' + seconds + 's ';
-
-    if (distance < 0) {
-      clearInterval(x);
-      document.querySelector('div').innerHTML = 'Times up';
+        if (distance < 0) {
+          clearInterval(x);
+          document.querySelector('div').innerHTML = 'Times up';
+          new Notification('Times up');
+        }
+      });
+    } else {
+      alert('알람을 허용해 주세요');
     }
   });
 }
